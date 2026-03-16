2edbc155959e06e348c74d13715f7737a021e161
KatrineRiis <kari0002@stud.ek.dk>
2026-03-16 21:08:07 +0100

    productlist_v1

---
+++ js/productList.js
@@ -0,0 +1,77 @@
+const urlParams = new URLSearchParams(window.location.search);
+const selectedCategory = urlParams.get("category");
+const listContainer = document.querySelector(".product-gallery");
+const sortByPriceBtn = document.querySelector("#sortByPriceBtn");
+const selectedTag = urlParams.get("tag");
+
+let allProducts = [];
+
+function fetchCategoryData(category) {
+  const url = `https://dummyjson.com/products/category/${category}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      let products = data.products;
+      //bruge de tags i JSON til at fetch data specifikt til "tilbehør til dyr" --> tagget skrives i html a tag
+      if (selectedTag) {
+        products = products.filter((product) => product.tags.includes(selectedTag));
+      }
+      allProducts = products;
+      getProducts(allProducts);
+    });
+}
+
+function getProducts(products) {
+  listContainer.innerHTML = products
+    .map((product) => {
+      const price = product.price;
+      const discount = product.discountPercentage;
+      const stock = product.stock;
+
+      let discountedPrice;
+      if (discount) {
+        discountedPrice = price - (price * discount) / 100;
+      } else {
+        discountedPrice = price;
+      }
+
+      let priceHTML;
+      if (stock <= 0) {
+        priceHTML = `<span class="sold_out">Sold out</span>`;
+      } else if (discount) {
+        priceHTML = `
+          <span class="old_price"><s>${price} DKK</s></span>
+          <span class="discounted_price">${discountedPrice.toFixed(2)} DKK</span>
+        `;
+      } else {
+        priceHTML = `<span>${price} DKK</span>`;
+      }
+
+      let imageClass;
+      if (stock <= 0) {
+        imageClass = "productlist_img_soldout";
+      } else {
+        imageClass = "productlist_img";
+      }
+
+      return `
+        <article class="productlist_card">
+            <img class="${imageClass}" src="${product.images[0]}" alt="${product.title}">
+            <h3 class="productlist_title">${product.title}</h3>
+            <p class="productlist_price">${priceHTML}</p>
+            <a href="product.html?id=${product.id}" class="productlist_view">View Product</a>
+        </article>
+      `;
+    })
+    .join("");
+}
+
+function sortByPriceAsc() {
+  const sorted = [...allProducts].sort((a, b) => a.price - b.price);
+  getProducts(sorted);
+}
+
+sortByPriceBtn.addEventListener("click", sortByPriceAsc);
+
+fetchCategoryData(selectedCategory);
